9739cf1eaa3d3d9aecc19485d1f8d1f6388bc4a2
Robert Stelzmann <robert.stelzmann@isax.com>
2025-07-13 14:49:49 +0200

    Set Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ingredients-scan/', // Wichtig für GitHub Pages!
 })
